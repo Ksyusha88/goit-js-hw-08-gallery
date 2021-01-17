b6c850c93fb81c36bafac3c729abb831533b5088
Ksyusha88 <ksyusha.gorelova@gmail.com>
2021-01-17 23:39:21 +0200

    модальное окно

--- gallery.js
+++ gallery.js
@@ -19,7 +19,14 @@
 
 import img_array  from './gallery-items.js';
 
- const galleryRef = document.querySelector('.js-gallery');
+const refs = {
+   gallery:document.querySelector('.js-gallery'),
+   galleryImage:document.querySelector('gallery__image'),
+   modal: document.querySelector('.js-lightbox')
+   // btn:document.querySelector('[data-action="close-lightbox"]')
+}
+
+//  const galleryRef = document.querySelector('.js-gallery');
  const newItems = img_array.map(currentItem => {
 
     const galleryItem = document.createElement("li");
@@ -39,5 +46,70 @@ import img_array  from './gallery-items.js';
 
     return galleryItem;
  });
-    
- galleryRef.append(...newItems);  
+
+ refs.gallery.append(...newItems);  
+
+
+refs.gallery.addEventListener('click', onGalleryClick );
+
+function onGalleryClick(event){
+   event.preventDefault();
+
+   if(event.target.nodeName !== 'IMG') {
+      return;
+   }
+   // const imageRef = event.target;
+   // const galleryImageURL = imageRef.dataset.source;
+   // refs.galleryImage.src = galleryImageURL;
+   openModal();
+   // refs.modal.addEventListener("click", onModal);
+   // window.addEventListener("keydown", onModal);
+   
+   //console.log();
+}
+
+function openModal() {
+   refs.modal.classList.add("is-open");
+   refs.modal.querySelector("img");
+   refs.modal.setAttribute("src", event.target.dataset.source);
+   //refs.modal.setAttribute("src", galleryImageURL);
+
+   console.log(event.target.dataset.source);
+}
+
+function onModal() {
+   closeModal();
+ }
+
+ function closeModal() {
+    if(
+      event.target.dataset.action !== "close-lightbox" &&
+      !event.target.classList.contains("lightbox__overlay") &&
+      event.code !== "Escape"
+    ) {
+       return;
+       }
+    refs.modal.classList.remove("is-open");
+    refs.modal.removeEventListener("click", onModal);
+    window.removeEventListener("keydown", onModal);
+    refs.modal.querySelector("img").removeAttribute("src");
+   }
+
+
+// function onClickHandler(event) {
+//    event.preventDefault();
+   
+//    if(event.target.nodeName === 'IMG') {
+//       refs.lightbox.classList.add('is-open');
+//       // refs.lightbox.querySelector('.lightbox__image').src = event.target.src;
+//       // refs.lightbox.querySelector('.lightbox__image').alt = event.target.alt;
+// }
+ 
+// function onCloseHandler(event) {
+//    if(event.target.nodeName === "I" || event.target.nodeName === "BUTTON") {
+//    refs.lightbox.classList.remove('is-open');
+// }
+// }
+ 
+// // refs.gallery.addEventListener('click', onClickHandler);
+// // refs.btn.addEventListener('click', onCloseHandler);
